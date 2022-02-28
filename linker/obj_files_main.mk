83d7d7dd7228d74945d3ce68813c16a6df62edab
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-02-27 21:28:54 -0500

    decompile text_1.s

--- linker/obj_files_main.mk
+++ linker/obj_files_main.mk
@@ -6,33 +6,35 @@ SOURCES :=\
 	$(BUILD_DIR)/asm/main/sdata2.o\
 	$(BUILD_DIR)/src/main/Dolphin/__start.o\
 	$(BUILD_DIR)/asm/main/Dolphin/__ppc_eabi_init.o\
-    $(BUILD_DIR)/asm/main/Dolphin/__mem.o\
-    $(BUILD_DIR)/asm/main/Dolphin/mem_TRK.o\
-    $(BUILD_DIR)/asm/main/Dolphin/__exception.o\
-    $(BUILD_DIR)/asm/main/Dolphin/dolphin_trk.o\
+	$(BUILD_DIR)/asm/main/Dolphin/__mem.o\
+	$(BUILD_DIR)/asm/main/Dolphin/mem_TRK.o\
+	$(BUILD_DIR)/asm/main/Dolphin/__exception.o\
+	$(BUILD_DIR)/asm/main/Dolphin/dolphin_trk.o\
 	$(BUILD_DIR)/asm/main/rodata.o\
-    $(BUILD_DIR)/asm/main/text.o\
-    $(BUILD_DIR)/asm/main/data.o\
+	$(BUILD_DIR)/asm/main/text.o\
+	$(BUILD_DIR)/asm/main/data.o\
 	$(BUILD_DIR)/src/main/Dolphin/ansi_files.o\
 	$(BUILD_DIR)/asm/main/Dolphin/ansi_fp.o\
-	$(BUILD_DIR)/asm/main/data_1.o\
-    $(BUILD_DIR)/asm/main/text_1.o\
-    $(BUILD_DIR)/asm/main/Dolphin/direct_io.o\
-    $(BUILD_DIR)/asm/main/Dolphin/file_io.o\
-    $(BUILD_DIR)/asm/main/Dolphin/FILE_POS.o\
-    $(BUILD_DIR)/asm/main/Dolphin/locale.o\
+	$(BUILD_DIR)/src/main/Dolphin/arith.o\
+	$(BUILD_DIR)/src/main/Dolphin/buffer_io.o\
+	$(BUILD_DIR)/src/main/Dolphin/critical_regions.gamecube.o\
+	$(BUILD_DIR)/src/main/Dolphin/ctype.o\
+	$(BUILD_DIR)/asm/main/Dolphin/direct_io.o\
+	$(BUILD_DIR)/asm/main/Dolphin/file_io.o\
+	$(BUILD_DIR)/asm/main/Dolphin/FILE_POS.o\
+	$(BUILD_DIR)/asm/main/Dolphin/locale.o\
 	$(BUILD_DIR)/asm/main/rodata_1.o\
 	$(BUILD_DIR)/asm/main/data_2.o\
-    $(BUILD_DIR)/asm/main/Dolphin/mbstring.o\
-    $(BUILD_DIR)/src/main/Dolphin/mem.o\
-    $(BUILD_DIR)/asm/main/Dolphin/mem_funcs.o\
-    $(BUILD_DIR)/src/main/Dolphin/misc_io.o\
-    $(BUILD_DIR)/asm/main/text_2.o\
-    $(BUILD_DIR)/asm/main/extabindex.o\
-    $(BUILD_DIR)/asm/main/ctors.o\
-    $(BUILD_DIR)/asm/main/dtors.o\
-    $(BUILD_DIR)/asm/main/sdata.o\
-    $(BUILD_DIR)/asm/main/sbss_1.o\
-    $(BUILD_DIR)/asm/main/sdata2_1.o\
-    $(BUILD_DIR)/asm/main/sbss2.o\
-    $(BUILD_DIR)/asm/main/bss.o\
+	$(BUILD_DIR)/asm/main/Dolphin/mbstring.o\
+	$(BUILD_DIR)/src/main/Dolphin/mem.o\
+	$(BUILD_DIR)/asm/main/Dolphin/mem_funcs.o\
+	$(BUILD_DIR)/src/main/Dolphin/misc_io.o\
+	$(BUILD_DIR)/asm/main/text_2.o\
+	$(BUILD_DIR)/asm/main/extabindex.o\
+	$(BUILD_DIR)/asm/main/ctors.o\
+	$(BUILD_DIR)/asm/main/dtors.o\
+	$(BUILD_DIR)/asm/main/sdata.o\
+	$(BUILD_DIR)/asm/main/sbss_1.o\
+	$(BUILD_DIR)/asm/main/sdata2_1.o\
+	$(BUILD_DIR)/asm/main/sbss2.o\
+	$(BUILD_DIR)/asm/main/bss.o\
---
+++ src/main/Dolphin/critical_regions.gamecube.c
@@ -0,0 +1,32 @@
+#include "types.h"
+
+/*
+ * --INFO--
+ * Address:	800D1F78
+ * Size:	000004
+ */
+void __end_critical_region(void) { return; }
+
+/*
+ * --INFO--
+ * Address:	800D1F7C
+ * Size:	000004
+ */
+void __begin_critical_region(void) { return; }
+
+/*
+ * --INFO--
+ * Address:	800D1F80
+ * Size:	000004
+ */
+void __kill_critical_regions(void) { return; }
+
+/*
+ * --INFO--
+ * Address:	........
+ * Size:	000004
+ */
+void __init_critical_regions(void)
+{
+	// UNUSED FUNCTION
+}
